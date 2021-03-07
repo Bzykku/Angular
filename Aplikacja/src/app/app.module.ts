d8622408d7c71feb72ea80f722b09b678abcaef3
Kamil Błąkała <kamil.blakala@gmail.com>
2021-03-07 16:16:41 +0100

    Zadanie finalne - koniec

--- Aplikacja/src/app/app.module.ts
+++ Aplikacja/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
